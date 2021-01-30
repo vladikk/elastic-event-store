97e612eae58d9041ec6a9d50e2cc39e9d19c8240
Vladik Khononov <vladik@khononov.com>
2021-01-30 10:10:06 +0200

    add filtering to changeset fetching endpoint

--- src/ees/commands/changesets.py
+++ src/ees/commands/changesets.py
@@ -8,8 +8,13 @@ class FetchChangesets:
     def execute(self, event, context):
         query_string = event.get("queryStringParameters") or {}
         stream_id = query_string.get("stream_id")
+        to_changeset = query_string.get("to")
+        from_changeset = query_string.get("from")
         
-        changesets = self.db.fetch_stream_changesets(stream_id)
+        changesets = self.db.fetch_stream_changesets(
+            stream_id,
+            from_changeset=from_changeset,
+            to_changeset=to_changeset)
 
         changesets = [{ "changeset_id": c.changeset_id,
                         "events": c.events,
--- src/tests/integration/test_fetch_stream.py
+++ src/tests/integration/test_fetch_stream.py
@@ -5,8 +5,8 @@ from unittest import TestCase
 from tests.integration.apigateway_level_tests import ApiGatewayTest
 
 
-class TestFetchingStreams(ApiGatewayTest):
-    def test_version(self):
+class TestFetchingStream(ApiGatewayTest):
+    def test_fetch_changesets(self):
         stream_id = str(uuid.uuid4())
         url = self.api_endpoint + f'commit?stream_id={stream_id}'
 
@@ -38,6 +38,98 @@ class TestFetchingStreams(ApiGatewayTest):
                         { "type": "init", "foo": "bar" },
                         { "type": "update", "foo": "baz" },
                     ]
+                }
+            ]
+        })
+
+    def test_fetch_from_specic_changeset(self):
+        stream_id = str(uuid.uuid4())
+        url = self.api_endpoint + f'commit?stream_id={stream_id}'
+        metadata = { "metadata": "goes here" }
+        events = [ { "type": "init" }, { "type": "update" } ]
+        requests.post(url, json={"events": events, "metadata": metadata})
+
+        url = self.api_endpoint + f'commit?stream_id={stream_id}&expected_changeset_id=1'
+        metadata = { "metadata": "goes here 2" }
+        events = [ { "type": "update2" }, { "type": "delete" } ]
+        requests.post(url, json={"events": events, "metadata": metadata})       
+
+        url = self.api_endpoint + f'changesets?stream_id={stream_id}&from=2'
+        response = requests.get(url)
+        
+        self.assertDictEqual(response.json(), {
+            "stream_id": stream_id,
+            "changesets": [
+                {
+                    "changeset_id": 2,
+                    "metadata": { "metadata": "goes here 2" },
+                    "events": [ { "type": "update2" }, { "type": "delete" } ]
+                }
+            ]
+        })
+    
+    def test_fetch_to_specic_changeset(self):
+        stream_id = str(uuid.uuid4())
+        url = self.api_endpoint + f'commit?stream_id={stream_id}'
+        metadata = { "metadata": "goes here" }
+        events = [ { "type": "init" }, { "type": "update" } ]
+        requests.post(url, json={"events": events, "metadata": metadata})
+
+        url = self.api_endpoint + f'commit?stream_id={stream_id}&expected_changeset_id=1'
+        metadata = { "metadata": "goes here 2" }
+        events = [ { "type": "update2" }, { "type": "delete" } ]
+        requests.post(url, json={"events": events, "metadata": metadata})       
+
+        url = self.api_endpoint + f'changesets?stream_id={stream_id}&to=1'
+        response = requests.get(url)
+        
+        self.assertDictEqual(response.json(), {
+            "stream_id": stream_id,
+            "changesets": [
+                {
+                    "changeset_id": 1,
+                    "metadata": { "metadata": "goes here" },
+                    "events": [ { "type": "init" }, { "type": "update" } ]
+                }
+            ]
+        })
+    
+    def test_fetch_from_and_to_specic_changesets(self):
+        stream_id = str(uuid.uuid4())
+        url = self.api_endpoint + f'commit?stream_id={stream_id}'
+        metadata = { "metadata": "goes here" }
+        events = [ { "type": "init" }, { "type": "update" } ]
+        requests.post(url, json={"events": events, "metadata": metadata})
+
+        url = self.api_endpoint + f'commit?stream_id={stream_id}&expected_changeset_id=1'
+        metadata = { "metadata": "goes here 2" }
+        events = [ { "type": "update2" } ]
+        requests.post(url, json={"events": events, "metadata": metadata})       
+
+        url = self.api_endpoint + f'commit?stream_id={stream_id}&expected_changeset_id=2'
+        metadata = { "metadata": "goes here 3" }
+        events = [ { "type": "update3" } ]
+        requests.post(url, json={"events": events, "metadata": metadata})
+
+        url = self.api_endpoint + f'commit?stream_id={stream_id}&expected_changeset_id=3'
+        metadata = { "metadata": "goes here 4" }
+        events = [ { "type": "update4" } ]
+        requests.post(url, json={"events": events, "metadata": metadata})       
+
+        url = self.api_endpoint + f'changesets?stream_id={stream_id}&from=2&to=3'
+        response = requests.get(url)
+        
+        self.assertDictEqual(response.json(), {
+            "stream_id": stream_id,
+            "changesets": [
+                {
+                    "changeset_id": 2,
+                    "metadata": { "metadata": "goes here 2" },
+                    "events": [ { "type": "update2" } ]
+                }, {
+                    "changeset_id": 3,
+                    "metadata": { "metadata": "goes here 3" },
+                    "events": [ { "type": "update3" } ]
                 }
             ]
         })
