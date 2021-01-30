7f935390c7de7b062e6fd8d201bfb32bfe511f1b
Vladik Khononov <vladik@khononov.com>
2021-01-30 09:14:56 +0200

    encapsulate api gateway test setup

--- src/tests/integration/test_commit_stream.py
+++ src/tests/integration/test_commit_stream.py
@@ -1,5 +1,6 @@
 import os
 from unittest import TestCase
+from tests.integration.apigateway_level_tests import ApiGatewayTest
 
 import boto3
 import pytest
@@ -7,44 +8,7 @@ import requests
 import uuid
 
 
-class TestApiGateway(TestCase):
-    api_endpoint: str
-
-    @classmethod
-    def get_stack_name(cls) -> str:
-        stack_name = os.environ.get("AWS_SAM_STACK_NAME")
-        if not stack_name:
-            raise Exception(
-                "Cannot find env var AWS_SAM_STACK_NAME. \n"
-                "Please setup this environment variable with the stack name where we are running integration tests."
-            )
-
-        return stack_name
-
-    def setUp(self) -> None:
-        """
-        Based on the provided env variable AWS_SAM_STACK_NAME,
-        here we use cloudformation API to find out what the HelloWorldApi URL is
-        """
-        stack_name = TestApiGateway.get_stack_name()
-
-        client = boto3.client("cloudformation")
-
-        try:
-            response = client.describe_stacks(StackName=stack_name)
-        except Exception as e:
-            raise Exception(
-                f"Cannot find stack {stack_name}. \n" f'Please make sure stack with the name "{stack_name}" exists.'
-            ) from e
-
-        stacks = response["Stacks"]
-
-        stack_outputs = stacks[0]["Outputs"]
-        api_outputs = [output for output in stack_outputs if output["OutputKey"] == "ApiEndpoint"]
-        self.assertTrue(api_outputs, f"Cannot find output ApiEndpoint in stack {stack_name}")
-
-        self.api_endpoint = api_outputs[0]["OutputValue"]
-
+class TestApiGateway(ApiGatewayTest):
     def test_version(self):
         response = requests.get(self.api_endpoint + "version")
         self.assertDictEqual(response.json(), {"version": "0.0.1"})
